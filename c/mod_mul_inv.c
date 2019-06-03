e79cfe7966b5cc259bbf56f2c7baa8976a3ebf23
Kapandaria <gnudles@users.noreply.github.com>
2019-06-03 17:20:58 +0300

    Update mod_mul_inv.c

--- c/mod_mul_inv.c
+++ c/mod_mul_inv.c
@@ -1,18 +1,10 @@
 #include <stdint.h>
 //finds the modular multiplicative inverse of x over modulus of pow(2,bits)
-uint64_t find_mod_mul_inverse(uint64_t x, uint64_t bits)
+uint64_t find_mod_mul_inverse(uint64_t x, uint64_t bits, uint64_t * w)
 {
-        if (bits > 64 || ((x&1)==0))
+        if (bits > 64 || bits == 0 || ((x&1)==0))
                 return 0;
-        uint64_t mask;
-        if (bits == 64)
-                mask = -1;
-        else
-        {                
-                mask = 1;
-                mask<<=bits;
-                mask--;
-        }
+        uint64_t mask = ((uint64_t)-1)>>(64-bits);
         x&=mask;
         uint64_t result=1, state=x, ctz=0;
         while(state!=1ULL)
@@ -21,6 +13,38 @@ uint64_t find_mod_mul_inverse(uint64_t x, uint64_t bits)
                 result|=1ULL<<ctz;
                 state+=x<<ctz;
                 state&=mask;
+        }
+        if (w!=0)
+        {
+                if (bits>32)
+                {
+                uint64_t qmul[4];
+                uint64_t carryin, carryout;
+                qmul[0] = (result & 0xFFFFFFFF ) * (x & 0xFFFFFFFF);
+                qmul[1] = (result >> 32 ) * (x >> 32);
+                qmul[2] = (result & 0xFFFFFFFF ) * (x >> 32);
+                qmul[3] = (result >> 32 ) * (x & 0xFFFFFFFF);
+                carryin = 0;
+                qmul[0] += __builtin_addcll(qmul[0], qmul[2] << 32 , carryin, (unsigned long long *)&carryout);
+                carryin = carryout;
+                qmul[1] += __builtin_addcll(qmul[1], qmul[2] >> 32 , carryin, (unsigned long long *)&carryout);
+                carryin = 0;
+                qmul[0] += __builtin_addcll(qmul[0], qmul[3] << 32 , carryin, (unsigned long long *)&carryout);
+                carryin = carryout;
+                qmul[1] += __builtin_addcll(qmul[1], qmul[3] >> 32 , carryin, (unsigned long long *)&carryout);
+                        if (bits == 64)
+                        {
+                                *w = qmul[1];
+                        }
+                        else
+                        {
+                                *w = (qmul[0]>>bits) | (qmul[1]<<(64-bits));
+                        }
+                }
+                else
+                {
+                        *w = (x*result)>>bits;
+                }
         }
         return result;
 }
